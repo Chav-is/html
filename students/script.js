47307585992270cd7971a4d75f2ef46d849ca0bf
mdean808 <unicorncalcs@gmail.com>
2017-11-18 10:51:02 -1000

    Added Student Page JS

--- students/script.js
+++ students/script.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
 
   $.get( "grabStudentName.php", { uid: getParameterByName('uid') } ).done(function( data ) {
       $('#studentName').append(data);
+      console.log(data);
   });
 
 
@@ -17,4 +18,59 @@ function getParameterByName(name, url) {
     if (!results) return null;
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+
+var loggedIn = false;
+var $loginToast = $('<span>Successfully Logged In!</span>').add($('<button onclick="Materialize.Toast.removeAll();" class="btn-flat toast-action">Yay!</button>'));
+function logIn() {
+	loggedIn = true;
+	$('.editable').attr('contenteditable','true');
+	Materialize.toast($loginToast, 4000)
+}
+
+function startEditing(element) {
+	if (loggedIn === true) {
+		$(element).addClass('editing')
+	}
+}
+
+function loadProfile() {
+	$.ajax({
+		method: 'post',
+		url: '/grabStudentName',
+		data: {
+			uuid: getParameterByName('id')
+		},
+		success: function (profile) {
+			console.log(profile);
+			$('#name').val('John');
+			var projects = profile.projectsJSON;
+			var projectAmount = projects.length;
+			for(var i = 0; i < projectAmount; i++) {
+				$('#project-cards').append('<div class="card horizontal hoverable">\n' +
+					'            <div class="card-image center" style="padding-left: 10px; margin-top: 10px;">\n' +
+					'                <img width="86" height="86" src="https://maxcdn.icons8.com/Share/icon/Logos//google_logo1600.png">\n' +
+					'                <h6>Student Corner</h6>\n' +
+					'            </div>\n' +
+					'            <div class="card-stacked">\n' +
+					'                <div class="card-content">\n' +
+					'                    <p>This is an short, but helpful project description. </p>\n' +
+					'                    <a href="#" style="margin-top: 40px;" class="btn red waves-effect">Go</a>\n' +
+					'                </div>\n' +
+					'            </div>\n' +
+					'        </div>')
+			}
+		},
+		error: function (result, err) {
+			console.log(err);
+			console.log(result);
+		}
+	})
+}
+
+function submitEdit() {
+	if (!$(".editable").is(":focus")) {
+		$('.editable').attr('contenteditable','false');
+	}
 }
